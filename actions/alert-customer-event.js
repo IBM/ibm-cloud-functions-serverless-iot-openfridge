856e9064fba105452770b91dc1dfa7b5a607034d
Daniel Krook <krook@us.ibm.com>
2017-03-20 11:01:55 -0700

    Fix promises in alert action.

--- actions/alert-customer-event.js
+++ actions/alert-customer-event.js
@@ -31,56 +31,67 @@ function main(params) {
   // Inspect the params sent to this action: Either invoked directly from check-warranty-renewal or via change to the 'order' database.
   // console.log(params);
 
-  return new Promise(function(resolve, reject) {
-
-    if (params.hasOwnProperty('appliance')) {
-      // Their warranty will expire soon.
-      // Invoked manually by the check-warranty-renewal action.
-      console.log('[alert-customer-event.main] invoked by check-warranty-renewal');
-      var appliance = params.appliance;
-      email = appliance.owner_email;
-      subject = 'Warranty expires soon for ' + appliance.serial;
-      content = 'Hello ' + appliance.owner_name + ', ';
-      content += 'your appliance with serial number ' + appliance.serial + ' will expire on ' + format(appliance.warranty_expiration) + '. ';
-      content += 'Contact a representative to renew your warranty.';
+  if (params.hasOwnProperty('appliance')) {
+    // Their warranty will expire soon.
+    // Invoked manually by the check-warranty-renewal action.
+    console.log('[alert-customer-event.main] invoked by check-warranty-renewal');
+    var appliance = params.appliance;
+    email = appliance.owner_email;
+    subject = 'Warranty expires soon for ' + appliance.serial;
+    content = 'Hello ' + appliance.owner_name + ', ';
+    content += 'your appliance with serial number ' + appliance.serial + ' will expire on ' + format(appliance.warranty_expiration) + '. ';
+    content += 'Contact a representative to renew your warranty.';
+    return send(email, subject, content, params)
+      .then(res => ({
+        success: '[alert-customer-event.main] Success sending notification'
+      }))
+      .catch(err => ({
+        error: '[alert-customer-event.main] Error sending notification: ' + err
+      }));
+  } else {
+    // Triggered by a change event in the order database.
+    var order = params;
+    if (order.status == 'ordered') {
+      // The order was automatically placed for them (in warranty).
+      console.log('[alert-customer-event.main] triggered by a newly created order under warranty.');
+      email = order.owner_email;
+      subject = 'Part automatically ordered for appliance: ' + order.appliance_serial;
+      content = 'Your appliance told us that one of its parts needed a replacement. Since it is still under warranty until ';
+      content += format(order.appliance_warranty_expiration) + ', we have automatically ordered a replacement. ';
+      content += 'It will be delivered soon.';
       return send(email, subject, content, params)
-        .then(res => ({success: '[alert-customer-event.main] Success sending notification'}))
-        .catch(err => ({ error: '[alert-customer-event.main] Error sending notification: ' + err }));
+        .then(res => ({
+          success: '[alert-customer-event.main] Success sending notification'
+        }))
+        .catch(err => ({
+          error: '[alert-customer-event.main] Error sending notification: ' + err
+        }));
+    } else if (order.status == 'pending') {
+      // The order was entered in pending mode (not in warranty).
+      console.log('[alert-customer-event.main] triggered by a newly pending order out of warranty.');
+      email = order.owner_email;
+      subject = 'Part ready to order for appliance: ' + order.appliance_serial;
+      content = 'Your appliance told us that one of its parts needed a replacement. Since it is no longer under warranty ';
+      content += '(it expired on ' + format(order.appliance_warranty_expiration) + '), you will need to approve the pending order. ';
+      content += 'Complete the form with your payment and the part will be on its way soon.';
+      return send(email, subject, content, params)
+        .then(res => ({
+          success: '[alert-customer-event.main] Success sending notification'
+        }))
+        .catch(err => ({
+          error: '[alert-customer-event.main] Error sending notification: ' + err
+        }));
     } else {
-      // Triggered by a change event in the order database.
-      var order = params;
-      if (order.status == 'ordered') {
-        // The order was automatically placed for them (in warranty).
-        console.log('[alert-customer-event.main] triggered by a newly created order under warranty.');
-        email = order.owner_email;
-        subject = 'Part automatically ordered for appliance: ' + order.appliance_serial;
-        content = 'Your appliance told us that one of its parts needed a replacement. Since it is still under warranty until ';
-        content += format(order.appliance_warranty_expiration) + ', we have automatically ordered a replacement. ';
-        content += 'It will be delivered soon.';
-        return send(email, subject, content, params)
-          .then(res => ({success: '[alert-customer-event.main] Success sending notification'}))
-          .catch(err => ({ error: '[alert-customer-event.main] Error sending notification: ' + err }));
-      } else if (order.status == 'pending') {
-        // The order was entered in pending mode (not in warranty).
-        console.log('[alert-customer-event.main] triggered by a newly pending order out of warranty.');
-        email = order.owner_email;
-        subject = 'Part ready to order for appliance: ' + order.appliance_serial;
-        content = 'Your appliance told us that one of its parts needed a replacement. Since it is no longer under warranty ';
-        content += '(it expired on ' + format(order.appliance_warranty_expiration) + '), you will need to approve the pending order. ';
-        content += 'Complete the form with your payment and the part will be on its way soon.';
-        return send(email, subject, content, params)
-          .then(res => ({success: '[alert-customer-event.main] Success sending notification'}))
-          .catch(err => ({ error: '[alert-customer-event.main] Error sending notification: ' + err }));
-      } else {
-        // Some other order status we're not implementing at the moment.
-        console.log('[alert-customer-event.main] triggered by some other order status.');
+      // Some other order status we're not implementing at the moment.
+      console.log('[alert-customer-event.main] triggered by some other order status.');
+      return new Promise(function(resolve, reject) {
         resolve({
           result: 'This workflow is not yet implemented.'
         });
-      }
+      });
     }
+  }
 
-  });
 }
 
 // Convert from Unix timestamp
